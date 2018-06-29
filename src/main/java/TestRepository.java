b643800e515102dac7b88312066acc78fbf6515a
Unknown <nguyenngocchau18@gmail.com>
2018-06-29 09:36:41 +0700

    Add demo artifactory publish repository

---
+++ src/main/java/TestRepository.java
@@ -0,0 +1,13 @@
+public class TestRepository {
+
+    /** Combine 2 string
+     *
+     * @param param1 String 1
+     * @param param2 String 2
+     * @return combine of 2 string
+     */
+    private static String addString(String param1, String param2) {
+        return param1 + param2;
+    }
+
+}
